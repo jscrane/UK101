a19c69e3397a088367a05565cec9b175c6de4e9c
Stephen Crane <jscrane@gmail.com>
2014-11-17 15:09:29 +0000

    mixin Memory::Device

--- display.h
+++ display.h
@@ -1,7 +1,7 @@
 #ifndef _DISPLAY_H
 #define _DISPLAY_H
 
-class display: public UTFTDisplay {
+class display: public UTFTDisplay, public Memory::Device {
 public:
 	virtual void operator= (byte c) { _set(_acc, c); }
 	virtual operator byte () { return _mem[_acc]; }
@@ -9,7 +9,7 @@ public:
 	virtual void checkpoint(Stream &s);
 	virtual void restore(Stream &s);
 
-	display() : UTFTDisplay(sizeof(_mem)), _resolution(0) {}
+	display(): Memory::Device(sizeof(_mem)), _resolution(0) {}
 	void begin();
 	const char *changeResolution();
 
