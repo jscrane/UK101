d0fcd621078350a1242498216f70432bc3709440
steve <jscrane@gmail.com>
2023-07-25 14:23:45 +0100

    updates for LilyGO

--- uk101.ino
+++ uk101.ino
@@ -6,7 +6,7 @@
 #include <r6502.h>
 
 #include "config.h"
-#include "display.h"
+#include "screen.h"
 #include "ukkbd.h"
 #include "acia.h"
 #include "tape.h"
@@ -67,14 +67,14 @@ tape tape(files);
 acia acia(tape);
 
 ukkbd kbd;
-display disp;
+screen disp;
 r6502 cpu(memory);
 PWM pwm;
 
 void reset() {
 	bool sd = hardware_reset();
 
-	kbd.reset();	
+	kbd.reset();
 	disp.begin();
 #if defined(PWM_SOUND)
 	pwm.begin(PWM_SOUND);
