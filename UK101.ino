37cfd85cc6b22e2171c065d44170556aadd40d2c
steve <jscrane@gmail.com>
2023-10-20 10:13:53 +0100

    support both sides of a single drive

--- UK101.ino
+++ UK101.ino
@@ -67,8 +67,8 @@ flash_filer files(PROGRAMS);
 audio_filer audio(files);
 tape tape(audio);
 
-flash_file drive_a(1);
-disk disk(drive_a);
+flash_file drive_a(1), drive_c(2);
+disk disk(drive_a, drive_c);
 disk_timer disk_timer;
 
 ukkbd kbd;
@@ -80,6 +80,8 @@ void reset() {
 
 	kbd.reset();
 	screen.begin();
+	files.stop();
+	files.start();
 	disk.reset();
 
 	if (!sd)
@@ -124,8 +126,8 @@ void loop() {
 	static bool cpu_debug;
 #endif
 	static const char *filename;
-	static uint8_t device;
-	static const char *device_names[] = { "Tape", "A:", "B:" };
+	static uint8_t fd;
+	static const char *device_names[] = { "Tape", "A:", "C:", 0 };
 
 	if (ps2.available()) {
 		unsigned scan = ps2.read2();
@@ -162,9 +164,11 @@ void loop() {
 					files.restore(filename);
 				break; 
 			case PS2_F8:
-				device = (device + 1) % MAX_FILES;
-				files.select(device);
-				screen.status(device_names[device]);
+				fd++;
+				if (!device_names[fd])
+					fd = 0;
+				files.select(fd);
+				screen.status(device_names[fd]);
 				break;
 #if defined(CPU_DEBUG)
 			case PS2_F10:
--- disk.h
+++ disk.h
@@ -3,7 +3,7 @@
 
 class disk: public Memory::Device, public PIA, public ACIA {
 public:
-	disk(flash_file &f): Memory::Device(Memory::page_size), ACIA(f), _f(f), pos(0), track(0xff), ticks(0) {}
+	disk(flash_file &a, flash_file &c): Memory::Device(Memory::page_size), ACIA(a), driveA(a), driveC(c), drive(&a), pos(0), track(0xff), ticks(0) {}
 
 	void reset();
 	void tick();
@@ -23,7 +23,7 @@ private:
 	void seek_start();
 	void write(uint8_t);
 
-	flash_file &_f;
+	flash_file &driveA, &driveC, *drive;
 	uint32_t pos;
 	uint8_t track;
 
